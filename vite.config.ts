d0a55f8ac600661a911c0d455da4a8067da1205b
Mustafa Almheimid <130401790+MustafaAlmheimid@users.noreply.github.com>
2026-05-16 17:09:31 +0300

    edit vit.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(async () => {
-  const plugins = [react(), tailwindcss()];
-  try {
-    // @ts-ignore
-    const m = await import('./.vite-source-tags.js');
-    plugins.push(m.sourceTags());
-  } catch {}
-  return { plugins };
+export default defineConfig({
+  plugins: [react()],
+  
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 10000,
+    allowedHosts: [
+      'syrian-students-forum-leb.onrender.com'
+    ]
+  }
 })
